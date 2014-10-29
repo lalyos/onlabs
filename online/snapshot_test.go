decdd7f7e58cd72353028a9c8231101814a61f72
lalyos <lalyos@yahoo.com>
2014-10-29 21:13:52 +0100

    Snapshot tests made real

--- online/snapshot_test.go
+++ online/snapshot_test.go
@@ -7,7 +7,17 @@ import (
 
 func TestUnmarshallSnapshot(t *testing.T) {
 	b := []byte(`{
-		"name": "myname"
+			"base_volume": {
+				"id": "09a4184c-733b-43c8-99c3-f1dde30536fe",
+				"name": "vol simple snapshot"
+			},
+			"creation_date": "2014-05-22T12:11:06.055998+00:00",
+			"id": "6f418e5f-b42d-4423-a0b5-349c74c454a4",
+			"name": "snapshot-0-1",
+			"organization": "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+			"size": 10000000000,
+			"state": "snapshotting",
+			"volume_type": "l_hdd"
 		}`)
 
 	// d as domain
@@ -17,9 +27,19 @@ func TestUnmarshallSnapshot(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	exp := "myname"
+	exp := "snapshot-0-1"
 	if d.Name != exp {
 		t.Fatalf("Expected %#v, got %#v", exp, d.Name)
+	}
+
+	exp = "000a115d-2852-4b0a-9ce8-47f1134ba95a"
+	if d.Organization != exp {
+		t.Fatalf("Expected %#v, got %#v", exp, d.Organization)
+	}
+
+	expSize := 10000000000
+	if d.Size != expSize {
+		t.Fatalf("Expected %#v, got %#v", expSize, d.Size)
 	}
 
 }
